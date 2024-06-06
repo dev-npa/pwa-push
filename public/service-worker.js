0e00c8b6cab091d00158d5082f300b2b5ed1f3c9
Nishant Painter <dev.nishantpainter@gmail.com>
2024-06-06 13:05:59 +0530

    Update application manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,9 @@
+self.addEventListener("activate", (event) => {});
+
+self.addEventListener("install", (event) => {});
+
+self.addEventListener("fetch", (event) => {});
+
 self.addEventListener("push", function (event) {
   const payload = event.data ? event.data.text() : "no payload";
 
